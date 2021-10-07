5765efef1290d1a24288b5ab6d6e16a6e3db2fed
ShawnWang <51041296+shawn120@users.noreply.github.com>
2021-10-07 13:57:32 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hanoi_cheating
-a cheating tool for the game of Tower of Hanoi
+A cheating tool for the game of Tower of Hanoi.
+
+Basic recursion practice.
+
+Input the numbers of disks, return every moves to complete the task within the minimum moves.
+
+Link of the game of Tower of Hanoi
+
+https://www.mathsisfun.com/games/towerofhanoi.html
---
+++ hanoi_cheating.py
@@ -0,0 +1,27 @@
+'''
+Go to the website below to try Tower of Hanoi Game!
+https://www.mathsisfun.com/games/towerofhanoi.html
+'''
+print ("How many disks you want to play with?")
+m = int(input ())
+
+def hanoi (n):
+    
+    piles = []
+    for i in range(n, 0, -1):
+        piles.append(i)
+    disk = {'1': piles, '2':[], '3':[]}
+
+    def start (n, disk, source, temp, target):
+        if n > 0:
+            start (n-1, disk, source, target, temp)
+            if disk[source]:
+                disk[target].append(disk[source].pop())
+                print("move from "+source+" to "+target)
+                start (n-1, disk, temp, source, target)
+                
+    start(n, disk, "1", "2", "3")
+    print("Congraduation! You made it!")
+    # print(disk)
+
+hanoi(m)
